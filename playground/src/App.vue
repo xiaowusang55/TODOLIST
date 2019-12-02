909add7244bd2e7f0d9f799e8e16848cae1a3250
xiaowusang55 <xiaowusang@gmail.com>
2019-12-02 23:13:58 +0800

    update

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,16 +1,35 @@
 <template>
   <div id="app">
-
+    <BaseInput 
+      :value="inputValue"
+      @input="inputValue = $event"
+      placeholder="enter something"
+      type="text"
+      @focus="focusFn"
+    ></BaseInput>
+    <h1>{{inputValue}}</h1>
+    <em>{{msg}}</em>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BaseInput from './components/BaseInput.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    BaseInput
+  },
+  data() {
+    return {
+      inputValue: '',
+      msg: ''
+    }
+  },
+  methods: {
+    focusFn() {
+      window.console.log('parent vue');
+    }
   }
 }
 </script>
---
+++ playground/src/components/BaseInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <label>
+    <input
+      :value="value"   
+      @input="$emit('input', $event.target.value)"
+      v-bind="$attrs"
+      v-on="inputListeners"
+    >
+  </label>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  name: 'BaseInput',
+  props: ['value'],
+  computed: {
+    inputListeners() {
+      let vm = this;
+      return Object.assign({}, this.$listeners, {
+        input: function(event) {
+          vm.$emit('input', event.target.value)
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
